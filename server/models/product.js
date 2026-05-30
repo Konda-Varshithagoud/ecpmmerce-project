29283ef69c4799ef4242a84374f93d5c6a843eca
Varshitha <varshithagoud02@gmail.com>
2026-05-30 10:06:46 +0530

    fix product model path issue

--- server/models/product.js
+++ server/models/product.js
@@ -55,5 +55,6 @@ const productSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+const Product = mongoose.model("Product", productSchema);
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = Product;
